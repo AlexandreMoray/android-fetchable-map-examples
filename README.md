fe888bff314719d1eac26da9b4975f6eca6d5a95
Luc <lcouvreux@takima.fr>
2020-07-17 11:19:46 +0200

    feat: android examples

---
+++ README.md
@@ -0,0 +1,23 @@
+# Jawg Maps on Android examples
+
+This repository contains multiple examples on how to integrate Jawg Maps on your mobile app with Mapbox GL Android. 
+
+Don't forget to set your personal acces token in [strings.xml](./app/src/main/res/values/strings.xml). 
+
+## Examples description : 
+
+[SimpleMapActivity](./app/src/main/java/io/jawg/SimpleMapActivity.kt) : Displaying a simple dynamic map. 
+
+[PopupMapActivity](./app/src/main/java/io/jawg/PopupMapActivity.kt) : Adding a popup with markers on a map. 
+
+[GeometryMapActivity](./app/src/main/java/io/jawg/GeometryMapActivity.kt) : Adding geometry on your map with GeoJSON. 
+
+[SwitchLanguagesActivity](./app/src/main/java/io/jawg/SwitchLanguagesActivity.kt) : Changing your map's language. 
+
+[DifferentStylesActivity](./app/src/main/java/io/jawg/DifferentStylesActivity.kt) : Changing your map style using our default styles.
+
+[CustomStyleActivity](./app/src/main/java/io/jawg/CustomStyleActivity.kt) : Using a custom style from [Jawg Lab](https://jawg.io/lab) on your map. 
+
+>You'll need to set a style ID in `CustomStyleActivity` to make this example work. 
+>
+> If you don't have any style ID yet, go to [Jawg Lab](https://jawg.io/lab/styles) to create one or read [this documentation](https://jawg.io/docs/maps#get-custom-style-id) for more informations.  
---
+++ app/src/main/java/io/jawg/MainActivity.kt
@@ -0,0 +1,46 @@
+package io.jawg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(getString(R.string.jawg_access_token)=="") {
+            Toast.makeText(applicationContext, "Please enter your access token in strings.xml", Toast.LENGTH_LONG).show();
+        }
+        buttonToClassicMap.setOnClickListener{
+            val intent = Intent(this, SimpleMapActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToStylesMap.setOnClickListener{
+            val intent = Intent(this, DifferentStylesActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToGeometryMap.setOnClickListener{
+            val intent = Intent(this, GeometryMapActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToPopupMap.setOnClickListener{
+            val intent = Intent(this, PopupMapActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToLanguageMap.setOnClickListener{
+            val intent = Intent(this, SwitchLanguagesActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonToCustomMap.setOnClickListener{
+            val intent = Intent(this, CustomStyleActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
